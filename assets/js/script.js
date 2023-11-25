43899884ab389e1b29895f86c7400f923c6ee2e5
Novak030 <>
2023-11-25 17:51:32 +0000

    add questions array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,26 @@
+//Global Variables
 let points;
 let numberTurns;
 let countTurns;
 
+
+//Function to start quiz
 function startQuiz() {
     points = 0;
-    numberTurns = 3;
+    numberTurns = 5;
     countTurns = 0;
+
+
+}
+
+
+//Questions Array
+
+function createQuestions() {
+    arrayQuestions[0] =
+    arrayQuestions[1] =
+    arrayQuestions[2] =
+    arrayQuestions[3] =
+    arrayQuestions[4] =
+
 }
