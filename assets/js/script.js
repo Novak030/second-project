1342d05ffe3298dfb7b663502fd8deca05b13019
Novak030 <>
2023-11-25 18:03:19 +0000

    add shift method to get questions, add split method to sort

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,10 +17,16 @@ function startQuiz() {
 //Questions Array
 
 function createQuestions() {
-    arrayQuestions[0] =
-    arrayQuestions[1] =
-    arrayQuestions[2] =
-    arrayQuestions[3] =
-    arrayQuestions[4] =
+    arrayQuestions[0] = "The first moon landing occurred in which year?# 1980# 1956# 1969# 1969"; 
+    arrayQuestions[1] = "When did the Berlin Wall fall?# 1995# 1989# 1980# 1989";
+    arrayQuestions[2] = "In which year did the Titanic sink?# 1912# 1923# 1905# 1912";
+    arrayQuestions[3] = "When did World War II officially start?# 1936# 1939# 1945# 1939";
+    arrayQuestions[4] = "The French Revolution began in which year?# 1689# 1789# 1889# 1789";
 
+}
+
+//Function how Game works 
+function startTurn() {
+    let currQuestion = arrayQuestions.shift();
+    const arrayQuestionsProcessed = currQuestion.split("#");
 }
